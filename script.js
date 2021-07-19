273467a3cfc69fe69ef4e5a650beaffee5cd35b5
Hebert324 <hebertrocha76@gmail.com>
2021-07-19 20:46:53 -0300

    part of knowledge completed

---
+++ script.js
@@ -0,0 +1,27 @@
+const text = document.querySelector(".effect-text2")
+
+function typeWriter(elemento) {
+    const textArray1 = elemento.innerHTML.split('')
+    elemento.innerHTML = ''
+    textArray1.forEach((letra, i) => {
+        setTimeout(() => {
+            elemento.innerHTML += letra
+        }, 75 * i)
+    })
+}
+
+typeWriter(text)
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+  })
+
+sr.reveal(
+    `.wrapper, .divider, .cards, .card,
+    #about, .image, #about, .text,
+    .services, .skill, .skills-data`,
+    { delay: 100 }
+)
