957792dc2f099c8a64c2ab1871df4586f9d66f78
Hebert324 <hebertrocha76@gmail.com>
2021-07-20 12:54:47 -0300

    burger menu completed and project completed

--- script.js
+++ script.js
@@ -25,3 +25,23 @@ sr.reveal(
     .services, .skill, .skills-data, .contact, input`,
     { delay: 100 }
 )
+
+// menu mobile
+
+const nav = document.querySelector('#header')
+const toggle = document.querySelectorAll('.menu-mobile, .menu-mobile-x')
+
+for (const element of toggle) {
+    element.addEventListener('click',() => {
+        //Ele esta pegando o nav e vendo a lista de classes dele e adicionando ou tirando a classe show nele.
+        nav.classList.toggle('show')
+    })
+}
+
+const links = document.querySelectorAll('.menu ul li a')
+
+for(const link of links) {
+    link.addEventListener('click',() => {
+        nav.classList.remove('show')
+    })
+}
